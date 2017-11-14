f4b94a5f4e057f768b4f89cf21141b4aaa4bf89c
Erzi Hutama Dwirama Putra <32395231+erzihutama@users.noreply.github.com>
2017-11-14 21:35:58 +0700

    Add files via upload

---
+++ PBO11_10116479_Latihan2/src/PBO11_10116479_LATIHAN/pbo11_10116479_latihan2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO11_10116479_LATIHAN;
+
+/**
+ *
+ * @author ERZI HUTAMA
+ */
+public class pbo11_10116479_latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
