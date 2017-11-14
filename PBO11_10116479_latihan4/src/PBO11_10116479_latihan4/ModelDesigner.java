7777e64d89baf8b0304b7a48e5171189951080a1
Erzi Hutama Dwirama Putra <32395231+erzihutama@users.noreply.github.com>
2017-11-14 21:38:55 +0700

    Add files via upload

---
+++ PBO11_10116479_latihan4/src/PBO11_10116479_latihan4/ModelDesigner.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO11_10116479_latihan4;
+
+/**
+ *
+ * @author ERZI HUTAMA
+ */
+public class ModelDesigner extends ModelEmployee {
+
+    int bonus = 50000;
+
+    public void bonusDesigner() {
+
+        System.out.println("Salary Employee Designer : " + salary);
+        System.out.println("Bonus Designer : " + bonus);
+
+    }
+
+}
---
+++ PBO11_10116479_latihan4/src/PBO11_10116479_latihan4/ModelSystemAnalyst.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO11_10116479_latihan4;
+
+/**
+ *
+ * @author ERZI HUTAMA
+ */
+public class ModelSystemAnalyst extends ModelEmployee {
+
+    int bonus = 90000;
+
+    public void bonusSystemAnalyst() {
+        System.out.println("Salary Employee Designer : " + salary);
+        System.out.println("Bonus SystemAnalyst : " + bonus);
+    }
+}
