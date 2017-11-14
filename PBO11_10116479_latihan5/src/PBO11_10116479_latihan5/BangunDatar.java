7ff2a9231803ecb76aaf482c147d38c74a2167c7
Erzi Hutama Dwirama Putra <32395231+erzihutama@users.noreply.github.com>
2017-11-14 21:40:01 +0700

    Add files via upload

---
+++ PBO11_10116479_latihan5/src/PBO11_10116479_latihan5/BangunDatar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO11_10116479_latihan5;
+
+/**
+ *
+ * @author ERZI HUTAMA
+ */
+public interface BangunDatar {
+    
+    public void hitungluas();
+    public void hitungkeliling();
+    
+}
